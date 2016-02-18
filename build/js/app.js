2a3373b4085cac8cefe77c747a1d707513fa09b6
Ex-Zy <serega1905@ukr.net>
2016-02-18 17:06:04 +0200

    add screen

--- build/js/app.js
+++ build/js/app.js
@@ -30,4 +30,36 @@ $(document).ready(function() {
 			"display": "none"
 		})
 	});
+	// social fixed
+	function stickySocial() {
+		if ($('.js-sticky').length) { 
+			var el = $('.js-sticky');
+			var stickyTop = $('.js-sticky').offset().top; 
+			var footerTop = $('.bottom').offset().top; 
+			var stickyHeight = $('.js-sticky').outerHeight();
+			// var limit = footerTop - stickyHeight;
+			var limit = footerTop + 215;
+			$(window).scroll(function(){ 
+
+				var windowTop = $(window).scrollTop();
+				if ($(window).width() >= 1150) {
+				
+					if (stickyTop < windowTop){
+						el.css({ position: 'fixed', top: 10 });
+						el.addClass('is-active');
+					}
+					else {
+						el.css('position','static');
+						el.removeClass('is-active');
+					}
+
+					if (limit < windowTop) {
+						var diff = limit - windowTop;
+						el.css({top: diff});
+					} 
+				} 
+			});
+		}
+	};
+	stickySocial();
 });
